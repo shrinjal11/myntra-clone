61d874a46c500b6d8d1702cf7b9d0e7bead79feb
shrinjal11 <writetoshrinjal9@gmail.com>
2024-06-16 19:27:39 +0530

    myntra clone

---
+++ assets/js/data.js
@@ -0,0 +1,109 @@
+let items=[
+    {
+        id:'001',
+        image : './assets/img/tshirt.jpg',
+        company : 'H&M',
+        title : 'Cotton polo tshirt',
+        dis_price : 'Rs. 2299',
+        price : 'Rs. 2599',
+        discount: '20%'
+    },
+    {
+        id:'002',
+        image : './assets/img/tshirt.jpg',
+        company : 'Puma',
+        title : 'Cotton tshirt',
+        dis_price : 'Rs. 2299',
+        price : 'Rs. 2599',
+        discount: '20%'
+    },
+    {
+        id:'003',
+        image : './assets/img/tshirt.jpg',
+        company : 'CK',
+        title : 'Pure Cotton tshirt',
+        dis_price : 'Rs. 4299',
+        price : 'Rs. 5599',
+        discount: '30%'
+    },
+    {
+        id:'005',
+        image : './assets/img/tshirt.jpg',
+        company : 'CK',
+        title : 'Pure Cotton tshirt',
+        dis_price : 'Rs. 4299',
+        price : 'Rs. 5599',
+        discount: '30%'
+    },
+    {
+        id:'006',
+        image : './assets/img/tshirt.jpg',
+        company : 'Roadster',
+        title : 'Pure Cotton tshirt',
+        dis_price : 'Rs. 1299',
+        price : 'Rs. 1399',
+        discount: '10%'
+    },
+    {
+        id:'007',
+        image : './assets/img/tshirt.jpg',
+        company : 'Roadster',
+        title : 'Pure Cotton tshirt',
+        dis_price : 'Rs. 1299',
+        price : 'Rs. 1399',
+        discount: '10%'
+    },
+    {
+        id:'008',
+        image : './assets/img/tshirt1.jpg',
+        company : 'H&M',
+        title : 'Cotton polo tshirt',
+        dis_price : 'Rs. 2299',
+        price : 'Rs. 2599',
+        discount: '20%'
+    },
+    {
+        id:'009',
+        image : './assets/img/tshirt1.jpg',
+        company : 'Puma',
+        title : 'Cotton tshirt',
+        dis_price : 'Rs. 2299',
+        price : 'Rs. 2599',
+        discount: '20%'
+    },
+    {
+        id:'010',
+        image : './assets/img/tshirt1.jpg',
+        company : 'CK',
+        title : 'Pure Cotton tshirt',
+        dis_price : 'Rs. 4299',
+        price : 'Rs. 5599',
+        discount: '30%'
+    },
+    {
+        id:'011',
+        image : './assets/img/tshirt1.jpg',
+        company : 'CK',
+        title : 'Pure Cotton tshirt',
+        dis_price : 'Rs. 4299',
+        price : 'Rs. 5599',
+        discount: '30%'
+    },
+    {
+        id:'012',
+        image : './assets/img/tshirt1.jpg',
+        company : 'Roadster',
+        title : 'Pure Cotton tshirt',
+        dis_price : 'Rs. 1299',
+        price : 'Rs. 1399',
+        discount: '10%'
+    },
+    {
+        image : './assets/img/tshirt1.jpg',
+        company : 'Roadster',
+        title : 'Pure Cotton tshirt',
+        dis_price : 'Rs. 1299',
+        price : 'Rs. 1399',
+        discount: '10%'
+    }
+]
---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+
+
+
+let itemContainer= document.querySelector('#item-container');
+
+
+let innerHTML='';
+items.forEach(item=>{
+    innerHTML+=`
+    <div class="col-lg-2 col-md-4 mb-4">
+        <div class="card rounded-0 border-0 shadow">
+            <img src="${item.image}" class="card-img-top rounded-0" alt="tshirt">
+            <div class="card-body">
+                <h6 class="card-title fw-bold mb-0">${item.company}</h6>
+                <p class="card-text fw-400">${item.title}</p>
+                <div>
+                    <span class="fw-bold">${item.dis_price}</span>
+                    <del class="fs-14">${item.price}</del>&nbsp;
+                    <span class="text-orange text-opacity-75 fs-14 fw-400">(${item.discount} Off)</span>
+                </div>
+                <div class="mt-2">
+                    <button class="btn btn-success w-100" onclick="addToCart(${item.id})">Add to Cart</button>
+                </div>
+            </div>
+        </div>
+     </div>
+    `
+})
+itemContainer.innerHTML=innerHTML;
+
+// Add to cart section starts
+
+let cartItems=[];
+function addToCart(itemId){
+cartItems.push(itemId)
+cartIcon()
+}
+
+function cartIcon(){
+    let cartNumber=document.getElementById('cart-number');
+    cartNumber.innerText=cartItems.length;
+}
